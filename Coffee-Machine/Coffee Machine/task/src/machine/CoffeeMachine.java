7082075cc9aa13e21ddd7ba134c0da1cfd6c5cb0
Nolan-James <nj@nolanjames.me>
2021-11-23 23:27:13 +0000

    Stage 5/6 complete

--- Coffee-Machine/Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee-Machine/Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -22,22 +22,34 @@ public class CoffeeMachine {
                     break;
                 case "buy":
                     System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
-                    int drink = scanner.nextInt();
+                    int drink = -1;
+                    String back = scanner.nextLine();
+                    if (back.equals("back")) {
+                        break;
+                    } else {
+                        drink = Integer.parseInt(back);
+                    }
+
 
                     if (drink == 1) {
                         Espresso espresso = new Espresso();
                         coffeeMachine.checkContentsOfEspresso(espresso);
+                        break;
                     } else if (drink == 2) {
                         Latte latte = new Latte();
                         coffeeMachine.checkContentsOfLatte(latte);
+                        break;
                     } else if (drink == 3) {
                         Cappuccino cappuccino = new Cappuccino();
                         coffeeMachine.checkContentsOfCappuccino(cappuccino);
+                        break;
                     }
                 case "fill":
                     coffeeMachine.fill(scanner);
+                    break;
                 case "take":
                     coffeeMachine.take();
+                    break;
                 case "exit":
                     break;
             }
